1449a5fbc82c0cb86f23bd6551b2112009e1c562
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-07-27 15:51:44 +0100

    list products

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -5,6 +5,10 @@ import api from '../../services/api';
 
 export default class Main extends Component {
 
+  state = {
+    product: [],
+  }
+
   componentDidMount(){
     this.loadProducts();
   }
@@ -12,10 +16,16 @@ export default class Main extends Component {
   loadProducts = async ()=>{
     const response = await api.get('/products');
 
-    console.log(response.data.docs);
+    this.setState({product:response.data.docs});
   }
 
   render() {
-    return <h1>Test</h1>;
+    return (
+      <div className='product-list'>
+          {this.state.product.map(product => (
+            <h2 key={product._id}>{product.title}</h2>
+          ))}
+      </div>
+    );
   }
 }
